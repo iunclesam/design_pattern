a6fd2899968306176052b33ffd6720804eed60de
xingxuan <xingxuan@163.com>
2021-01-28 09:43:52 +0800

    Null Object Pattern

---
+++ src/main/java/com/tutorialspoint/null_object/NullCustomer.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint.null_object;
+
+public class NullCustomer extends AbstractCustomer {
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+}
