eda777a82472147e99bc597745d335ee3eb7ca0e
John <john@example.com>
2021-02-19 09:40:24 +0800

    Flyweight Pattern

---
+++ src/main/java/com/tutorialspoint/flyweight/Shape.java
@@ -0,0 +1,5 @@
+package com.tutorialspoint.flyweight;
+
+public interface Shape {
+    void draw();
+}
---
+++ src/main/java/com/tutorialspoint/flyweight/ShapeFactory.java
@@ -0,0 +1,20 @@
+package com.tutorialspoint.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShapeFactory {
+
+    private static final Map<String, Circle> circleMap = new HashMap<>();
+
+    public static Shape getCircle(String color) {
+        Circle circle = circleMap.get(color);
+
+        if (circle == null) {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            System.out.println("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+}
