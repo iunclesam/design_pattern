79fdaa93f9f547a1c6d0a513a94d961a48f01cdc
John <john@example.com>
2021-02-26 17:11:49 +0800

    Decorator Pattern

--- src/main/java/com/tutorialspoint/decorator/DecoratorPatternDemo.java
+++ src/main/java/com/tutorialspoint/decorator/DecoratorPatternDemo.java
@@ -1,5 +1,15 @@
 package com.tutorialspoint.decorator;
 
+/**
+ * Decorator pattern allows a user to add new functionality to an existing object without altering its structure.
+ * This type of design pattern comes under structural pattern as this pattern acts as a wrapper to existing class.
+ *
+ * This pattern creates a decorator class which wraps the original class and provides additional functionality keeping class methods signature intact.
+ *
+ * We are demonstrating the use of decorator pattern via following example in which we will decorate a shape with some color without alter shape class.
+ *
+ * See <a href='https://www.tutorialspoint.com/design_pattern/decorator_pattern.htm'>Decorator Pattern</a>
+ */
 public class DecoratorPatternDemo {
     public static void main(String[] args) {
 
