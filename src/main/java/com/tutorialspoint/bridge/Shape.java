6eb4b9c33cadc5e222068a8a4db79e2e968795f5
John <john@example.com>
2021-03-03 09:00:59 +0800

    Bridge Pattern

---
+++ src/main/java/com/tutorialspoint/bridge/Shape.java
@@ -0,0 +1,11 @@
+package com.tutorialspoint.bridge;
+
+public abstract class Shape {
+    protected DrawAPI drawAPI;
+
+    protected Shape(DrawAPI drawAPI) {
+        this.drawAPI = drawAPI;
+    }
+
+    public abstract void draw();
+}
