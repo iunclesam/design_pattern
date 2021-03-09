0f61727b754ddf21c5cdcfd74da2f182c540c008
John <john@example.com>
2021-03-09 09:05:32 +0800

    Prototype Pattern

---
+++ src/main/java/com/tutorialspoint/prototype/Shape.java
@@ -0,0 +1,32 @@
+package com.tutorialspoint.prototype;
+
+public abstract class Shape implements Cloneable {
+
+    private String id;
+    protected String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public abstract void draw();
+
+    @Override
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
